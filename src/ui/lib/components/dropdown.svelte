c958d3fdffd9f8ac0ea803e468a1d7ca0de50886
sugjo <iluxa-com2001@yandex.ru>
2022-08-16 03:29:56 +0500

    added ui element: dropdown

---
+++ src/ui/lib/components/dropdown.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { fly } from 'svelte/transition';
+	import clickOutside from '../utils/clickOutside';
+
+	let isCheck = false;
+
+	const clickHandler = () => {
+		isCheck = !isCheck;
+	};
+</script>
+
+<div class="dropdown-btn" class:check={isCheck} on:click|stopPropagation={clickHandler}>
+	{#if isCheck}
+		<img src="../public/images/expand_less.svg" alt="" />
+	{:else}
+		<img src="../public/images/expand_more.svg" alt="" />
+	{/if}
+</div>
+{#if isCheck}
+	<div
+		transition:fly={{ duration: 120, y: -5 }}
+		class="dropdown"
+		use:clickOutside={() => (isCheck = false)}
+	>
+		<slot />
+	</div>
+{/if}
+
+<style>
+	.dropdown-btn {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		width: 35px;
+		height: 35px;
+		border-radius: var(--border-radius);
+		user-select: none;
+		font-size: 20px;
+	}
+
+	.dropdown-btn:hover {
+		background-color: var(--background-hover);
+	}
+
+	.dropdown {
+		position: absolute;
+		display: flex;
+		gap: 5px;
+		z-index: 10;
+		left: 0;
+		width: 100%;
+		background-color: var(--background-active);
+		padding: 15px;
+	}
+
+	img {
+		filter: invert(100%);
+		height: 30px;
+		width: 30px;
+	}
+</style>
