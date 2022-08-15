aa3af6f6d71b024dd263744ce4514fd0cd7e2218
sugjo <iluxa-com2001@yandex.ru>
2022-08-16 03:30:13 +0500

    added ui element: toggle

---
+++ src/ui/lib/components/toggle.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	export let isCheck = false;
+
+	const dispatch = createEventDispatcher();
+
+	const clickHandler = () => {
+		if (isCheck) {
+			isCheck = false;
+			dispatch('uncheck');
+		} else {
+			isCheck = true;
+			dispatch('check');
+		}
+	};
+</script>
+
+<div class="toggle" class:check={isCheck} on:click={clickHandler}>
+	<div class="circle" />
+</div>
+
+<style>
+	.toggle {
+		display: flex;
+		justify-content: flex-start;
+		border: var(--border-hover);
+		width: 45px;
+		border-radius: 25px;
+		user-select: none;
+		font-size: 14px;
+        padding: 2px;
+	}
+
+	.circle {
+		width: 15px;
+		height: 15px;
+		border-radius: 25px;
+		transition: 0.1s;
+		background-color: var(--color);
+	}
+
+	.check .circle {
+		background-color: var(--active-color);
+	}
+
+	.toggle:hover {
+		background-color: var(--background-hover);
+	}
+
+	.check {
+		justify-content: flex-end;
+	}
+</style>
