3fe5b7c96ad430263324c199b00d4da1d113fcc9
sugjo <iluxa-com2001@yandex.ru>
2022-08-16 03:29:14 +0500

    added ui element: chips

---
+++ src/ui/lib/components/chips.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	export let text;
+	export let radio;
+	export let isCheck = false;
+
+	const dispatch = createEventDispatcher();
+
+	const clickHandler = () => {
+		if (isCheck && !radio) {
+			isCheck = false;
+			dispatch('uncheck');
+		} else {
+			isCheck = true;
+			dispatch('check');
+		}
+	};
+</script>
+
+<div class="chips" class:check={isCheck} on:click={clickHandler}>
+	{text}
+</div>
+
+<style>
+	.chips {
+		border: var(--border-hover);
+		padding: 5px 16px;
+		border-radius: 25px;
+		user-select: none;
+		font-size: 14px;
+		transition: 0.1s;
+	}
+
+	.chips:hover {
+		background-color: var(--background-hover);
+	}
+
+	.check {
+		border: var(--border-active);
+	}
+</style>
