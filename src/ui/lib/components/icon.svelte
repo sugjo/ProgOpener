149a936c760af6d846bfdfa40943eb594523d495
sugjo <iluxa-com2001@yandex.ru>
2022-08-27 16:48:39 +0500

    add ui element: icon

---
+++ src/ui/lib/components/icon.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let src;
+</script>
+
+<img class="icon" {src} draggable="false" alt="" />
+
+<style>
+	img {
+		user-select: none;
+		filter: invert(100%);
+		height: 30px;
+		width: 30px;
+	}
+	.icon {
+		grid-area: icon;
+		margin: 0 15px;
+	}
+</style>
