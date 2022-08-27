272c7bfc6be94e70dc53f33aea1a19a2d7570de5
sugjo <iluxa-com2001@yandex.ru>
2022-08-27 16:47:56 +0500

    add ui element: button

---
+++ src/ui/lib/components/button.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let icon = null;
+</script>
+
+<button on:click>
+	<div class="button-body">
+		{#if icon}
+            <img draggable="false" src={icon} alt="" />
+        {/if}
+		<slot />
+	</div>
+</button>
+
+<style>
+	button {
+		border-radius: var(--border-radius);
+		transition: 0.2s;
+        padding: 0;
+	}
+
+	button:hover {
+		background-color: var(--background-hover);
+	}
+
+	button:active .button-body {
+		position: relative;
+		top: 1px;
+	}
+
+    img {
+        height: 30px;
+		padding: 5px;
+        width: auto;
+        filter: invert(100%);
+    }
+</style>
