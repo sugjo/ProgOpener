c36464e5b89f3f47b3212640e4e5297ea6f71fcd
sugjo <iluxa-com2001@yandex.ru>
2023-01-25 18:01:56 +0500

    refactoring: search toggle

--- src-tauri/src/utils/setup.rs
+++ src-tauri/src/utils/setup.rs
@@ -1,35 +1,28 @@
-use tauri::{App, AppHandle, GlobalShortcutManager, Manager, Window};
+use tauri::{window, App, AppHandle, GlobalShortcutManager, Manager};
 
 #[cfg(target_os = "windows")]
 pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let window = app.get_window("search").unwrap();
     search_toggle_register(app.app_handle());
-
-    window.emit("files_update", "files").unwrap();
 
     Ok(())
 }
 
 fn search_toggle_register(app: AppHandle) {
-    let window = app.get_window("search").unwrap();
-
     app.global_shortcut_manager()
-        .register("Alt + Space", move || {
-            let is_visible = window.is_visible().unwrap();
-            if is_visible {
-                hide_search(&window)
-            } else {
-                show_search(&window)
-            }
-        })
+        .register("Alt + Space", move || toggle_search(&app))
         .unwrap();
 }
 
-fn show_search(window: &Window) {
-    window.show().unwrap();
-    window.set_focus().unwrap();
-}
+fn toggle_search(app: &AppHandle) {
+    let window = app.get_window("search").unwrap();
+    let is_visible = window.is_visible().unwrap();
 
-fn hide_search(window: &Window) {
-    window.hide().unwrap();
+    if is_visible {
+        window.hide().unwrap();
+    } else {
+        window.show().unwrap();
+        window.set_focus().unwrap();
+    }
+
+    window.emit("search_toggled", !is_visible).unwrap();
 }
