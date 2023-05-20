80e16374f290816cbb33d91436f33d0a96e0f465
sugjo <Iluxa-com2001@yandex.ru>
2023-05-21 01:48:33 +0500

    refactoring: global export store types

--- src/shared/store/createTypedAsyncThunk.ts
+++ src/shared/store/createTypedAsyncThunk.ts
@@ -1,5 +1,3 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import { AppDispatch, RootState } from "@/app/store";
-
 export const createTypedAsyncThunk = createAsyncThunk.withTypes<{state: RootState; dispatch: AppDispatch;}>();
--- src/shared/store/useAppDispatch.ts
+++ src/shared/store/useAppDispatch.ts
@@ -1,5 +1,3 @@
 import { useDispatch } from "react-redux";
 
-import { AppDispatch } from "@/app/store";
-
 export const useAppDispatch = useDispatch<AppDispatch>;
