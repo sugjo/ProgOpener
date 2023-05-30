ab409de3d64e995e0fa521013e2845455f0b4394
sugjo <Iluxa-com2001@yandex.ru>
2023-05-30 17:15:36 +0500

    refactoring: remove old business logic

--- src/components/settings/ui/Path.tsx
+++ src/components/settings/ui/Path.tsx
@@ -1,31 +1,34 @@
-import { usePaths } from "@/shared/hooks/usePaths";
-
-import { useSettingsPaths } from "../hooks/useSettingsPaths";
+import { settingsModel } from "@/entities/settings";
+import { useActionCreators, useTypedSelector } from "@/shared/lib/store";
 
 const Path = () => {
-	const {paths} = usePaths();
-	const {addPathHandler, changePathHandler, removePathHandler, togglePathHandler} = useSettingsPaths();
+	const {	pathsIds, pathsMap } = useTypedSelector((store) => store.settings.path);
+	const actions = useActionCreators(settingsModel.actions.path);
 
-	const pathsList = paths.pathsIds.map((id) => {
-		const status = paths.pathsMap[id].status;
+	const changeHandler = (id: string) => () => actions.updateThunk(id);
+	const deleteHandler = (id: string) => () => actions.delete(id);
+	const addHandler = () => actions.addThunk();
+	const toggleHandler = (id: string) => () => actions.toggle(id);
+
+	const pathsList = pathsIds.map((id) => {
+		const status = pathsMap[id].status;
+		const isActive = pathsMap[id].status === "on";
 
 		return (
 			<li key={id}>
-				<button disabled={status === "error"} onClick={togglePathHandler(id)}>
-					{status === "on" && "✅"}
-					{status === "off" && "☑️"}
-					{status === "error" && "⚠️"}
+				<button disabled={status === "error"} onClick={toggleHandler(id)}>
+					{isActive? "✅" : "☑️"}
 				</button>
-				{paths.pathsMap[id].path}
-				<button onClick={removePathHandler(id)}>delete</button>
-				<button onClick={changePathHandler(id)}>change</button>
+				{pathsMap[id].path}
+				<button onClick={deleteHandler(id)}>delete</button>
+				<button onClick={changeHandler(id)}>change</button>
 			</li>
 		);
 	});
 
 	return (
 		<>
-			<button onClick={addPathHandler}>Add path</button>
+			<button onClick={addHandler}>Add path</button>
 			<ul>{pathsList}</ul>
 		</>
 	);
